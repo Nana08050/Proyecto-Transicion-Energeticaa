c03e5673b69213e75db1eb072375d1c922685ee3
Nana <marianavm0805@gmail.com>
2025-02-25 20:52:34 -0500

    Botones, mensajes

--- js/dashboard.js
+++ js/dashboard.js
@@ -27,3 +27,25 @@ const ctx = document.getElementById('myChart');
         }
       }
     });})()
+
+    Swal.fire({ 
+      title: "Bienvenidos",
+      text: "La revolucion del energetica del campo colombiano comienza ahora",
+      imageUrl: "./imagenes/solar-8244680_1280.jpg",
+      imageWidth: 400,
+      imageHeight: 200,
+      imageAlt: "Custom image",
+      color:'#084db3',
+      confirmButtonColor: "#090466" ,
+    });
+
+
+    function test(){  
+      Swal.fire({
+        confirmButtonText: `
+      <iframe width="400" height="400" src="https://www.youtube.com/embed/TzfnlPxCZv0?si=547rW20B-UAx4R_h&amp;controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      `,imageHeight: 500,
+        imageAlt: "A tall image",
+        confirmButtonColor:"#d1e3f8",
+      });
+    }
